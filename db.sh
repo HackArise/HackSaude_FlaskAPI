1dd892ca5a303747e155b67ca98a22abb382f5ff
Victor Moura <victor_cmoura@hotmail.com>
2018-06-29 19:12:40 -0300

    Adding user argument to db commands

--- db.sh
+++ db.sh
@@ -1,5 +1,5 @@
-dropdb hacksaude
-createdb hacksaude
+sudo -u postgres dropdb hacksaude
+sudo -u postgres createdb hacksaude
 sudo -u postgres psql -c "DROP USER IF EXISTS arise;"
 sudo -u postgres psql -c "CREATE USER arise WITH PASSWORD 'arise';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE hacksaude TO arise;"
